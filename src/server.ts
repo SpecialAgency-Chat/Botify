6a51cc74fb3833b1af3831645cc02904d4c15fc1
waki285 <suzuneu@suzuneu.com>
2023-09-23 15:06:14 +0200

    routing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import { Bindings } from "./interfaces";
+import Interactions from "./routes/interactions";
 
 const app = new Hono<Bindings>();
 
 app.get("/", (c) => {
   return c.text("Hello World!");
 });
+
+app.route("/interactions", Interactions);
 
 export default app;
